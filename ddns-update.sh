eb368d67d638c34ed2257adc87406d6a0fab2f70
dongkeonkim <dongkeonkim0810@gmail.com>
2025-01-29 16:12:43 +0900

    build: 빌드 수정

---
+++ ddns-update.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl -s "https://dynamicdns.park-your-domain.com/update?host=api&domain=dailytodocalendar.com&password=${DDNS_PASSWORD}"
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,11 @@ echo "Starting sshd..."
 sudo /usr/sbin/sshd
 echo "sshd start attempted"
 
+# DDNS 업데이트 스크립트를 백그라운드로 실행
+echo "Starting DDNS update process..."
+(while true; do /app/ddns-update.sh; sleep 1800; done) &
+echo "DDNS update process started"
+
 # Spring Boot 애플리케이션 실행
 exec java -Xms512m -Xmx1024m -XX:+UseG1GC \
     -Dspring.profiles.active=${SPRING_PROFILES_ACTIVE} \
