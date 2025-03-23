d2859a0733bd0015788fe481b8e159ea914d5fb2
dongkeonkim <dongkeonkim0810@gmail.com>
2025-03-23 18:50:49 +0900

    feat: entrypoint.sh 삭제

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Spring Boot 애플리케이션 실행
-exec java -Xms512m -Xmx1024m -XX:+UseG1GC \
-    -Dspring.profiles.active=${SPRING_PROFILES_ACTIVE} \
-    -Dspring.config.location=classpath:/application.yml,optional:/app/config/application.yml \
-    -jar app.jar
