2e13ef3453a82919c3f908a097860ade24771630
dongkeonkim <dongkeonkim0810@gmail.com>
2025-03-30 17:17:51 +0900

    feat: 비밀번호 유효성 수정

--- src/main/java/com/dailytodocalendar/api/auth/dto/SignUpRequest.java
+++ src/main/java/com/dailytodocalendar/api/auth/dto/SignUpRequest.java
@@ -18,8 +18,9 @@ public class SignUpRequest {
   @NotBlank(message = "비밀번호는 필수 항목입니다.")
   @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
   @Pattern(
-      regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
-      message = "비밀번호는 최소 하나의 문자와 숫자를 포함해야 합니다.")
+      regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$",
+      message = "비밀번호는 최소 하나의 문자, 숫자, 특수문자를 포함해야 합니다."
+  )
   private String password;
 
   @NotBlank(message = "이름은 필수 항목입니다.")
--- src/main/java/com/dailytodocalendar/api/member/dto/MemberUpdateDto.java
+++ src/main/java/com/dailytodocalendar/api/member/dto/MemberUpdateDto.java
@@ -19,8 +19,9 @@ public class MemberUpdateDto {
 
   @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하여야 합니다.")
   @Pattern(
-      regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,20}$",
-      message = "비밀번호는 최소 하나의 대문자, 소문자, 숫자 및 특수 문자를 포함해야 합니다.")
+      regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$",
+      message = "비밀번호는 최소 하나의 문자, 숫자, 특수문자를 포함해야 합니다."
+  )
   private String newPassword;
 
   @Override
