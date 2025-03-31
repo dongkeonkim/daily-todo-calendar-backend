8909579900f3dc3906dee90839a9016e5aceea39
dongkeonkim <dongkeonkim0810@gmail.com>
2025-03-31 22:34:21 +0900

    feat: 정규식 수정

--- src/main/java/com/dailytodocalendar/api/member/dto/MemberUpdateDto.java
+++ src/main/java/com/dailytodocalendar/api/member/dto/MemberUpdateDto.java
@@ -20,8 +20,7 @@ public class MemberUpdateDto {
   @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하여야 합니다.")
   @Pattern(
       regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$",
-      message = "비밀번호는 최소 하나의 문자, 숫자, 특수문자를 포함해야 합니다."
-  )
+      message = "비밀번호는 최소 하나의 문자, 숫자, 특수문자를 포함해야 합니다.")
   private String newPassword;
 
   @Override
